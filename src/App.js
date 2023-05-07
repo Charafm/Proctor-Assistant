86338cc6f326b1ce1a382887f84ffba0ae6c100c
Charaf Eddine Moujahid <charafeddine.mojahid@gmail.com>
2023-05-07 14:43:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Fem from "./pages/fem/Fem";
+import Home from "./pages/home/Home";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  BrowserRouter,
+  UserProfile,
+  OwnUserProfile,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {" "}
+      <Routes>
+        {" "}
+        <Route path="/" element={<Home />} />{" "}
+        <Route path="fem/*" element={<Fem />} />{" "}
+      </Routes>{" "}
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/fem/Fem.jsx
@@ -0,0 +1,10 @@
+import "./fem.scss"; 
+const Fem = () => {   
+    return (     
+    <div className="fem">       
+        <h1 className="title">Proctor Assistant</h1>
+    </div>   ); 
+}; 
+    
+
+    export default Fem;
